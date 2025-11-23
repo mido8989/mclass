6e5d97cf4225792b63c77efe77cd40edd7d52a2b
mido8989 <mohamedesakorea@gmail.com>
2025-11-23 17:07:01 +0900

    check items

--- src/main/java/com/example/demo/MemberService.java
+++ src/main/java/com/example/demo/MemberService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-
+// check
 @Service
 @RequiredArgsConstructor
 public class MemberService {
